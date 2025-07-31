bdb6310f307f39fae7a80b90d1ac8e511cc0556c
Somanathpatil <patilsomanath292@gmail.com>
2025-07-31 22:46:57 +0530

    this is commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,57 @@
-import { useState } from 'react'
-import './App.css'
+import { useCallback, useState } from 'react'
 
 function App() {
-const [length , setlength] = useState(8)
+  const [length, setLength] = useState(8)
+  const [numberAllowed, setNumberAllowed] = useState(false)
+  const [charAllowed, setCharAllowed] = useState(false)
+  const [password, setPassword] = useState("")
+  const passwordGenerator = useCallback(() => {
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if(numberAllowed) str+= "0123456789"
+    if(charAllowed) str+= "!@#$%^&*-+={}[]`~"
 
+    for(let i=1 ; i <= array.length; i++){
+      let char = Math.floor(Math.random()* str.length + 1)
+
+      pass = str.charAt(char)
+    }
+    setPassword(pass)
+
+
+  }, [length, numberAllowed, charAllowed, setPassword])
+   
   return (
     <>
-    <h1 className='text-4xl text-center'>Password Generator</h1>
+      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-6   my-8 bg-gray-500 text-orange-500'>
+        <h1 className='text-white text-center my-3  '>Password Generator</h1>
+          <div className='flex shadow rounded-lg mb-4 bg-white overflow-hidden'>
+            <input type="text"
+            value={password}
+            placeholder='password'
+            className='outline-none w-full py-1 px-3'
+            readOnly
+            />
+
+            <button
+            className='outline-none bg-blue-500 text-white px-3 py-0.5 shrink-0'
+            >copy</button>
+
+          </div>
+
+            <div className='flex text-sm gap-x-2'>
+              <div className='flex items-center gap-x-1'>
+              <input type="range"
+              min={6}
+              max={100}
+              value={length}
+              className='cursor-pointer'
+              />
+              <label>Length:{length}</label>
+              </div>
+            </div>
+
+      </div>
     </>
   )
 }
